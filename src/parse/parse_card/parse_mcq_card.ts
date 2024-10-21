279720e9281b819806e041143a85c22beb38c2af
Shreedhar <shreedharpandey000@gmail.com>
2024-10-21 15:19:30 +0545

    Added Validation for MCQ Card

--- src/parse/parse_card/parse_mcq_card.ts
+++ src/parse/parse_card/parse_mcq_card.ts
@@ -34,7 +34,13 @@ export class ParseMcqCard {
         facts: data.facts,
         bloomLevel: data.bloom_level,
       };
-      return mcqCard;
+      //   return mcqCard;
+      const isValid = this._validate(mcqCard);
+      if (isValid == true) {
+        return mcqCard;
+      } else {
+        return null;
+      }
     } catch (e) {
       return null;
     }
@@ -53,6 +59,47 @@ export class ParseMcqCard {
       return finalDisplayTitle;
     } else {
       return question;
+    }
+  }
+
+  /// validate mcq card
+  // 1. Check if atleast 1 correct answer exists
+  // 2. Length of answer shouldnt exceed  24 chars
+  // 3. Length of question shouldnt exceed 90 chars
+  // 4. If Any option is Empty
+  _validate(mcqCard: any) {
+    try {
+      let isQuestionValid = mcqCard.content.question.length <= 90;
+      if (isQuestionValid) {
+        throw new Error("Question length exceeded");
+      }
+
+      /// check if all are wrong answers
+      let exists = this._checkIfAllAnswersAreWrong(mcqCard.content.answers);
+      if (exists) {
+      } else {
+        throw new Error("Every answers are wrong");
+      }
+
+      /// check if answers are of length <40 or is 0
+      let answerWhoseLengthisGreaterThan40or0 = (
+        mcqCard.content.answers ?? []
+      ).find((e: any) => e.answer.length == 0 || e.answer.length > 40);
+      if (answerWhoseLengthisGreaterThan40or0) {
+        throw new Error("Option has length more than 40 or is Empty");
+      }
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  _checkIfAllAnswersAreWrong(answers: any[]) {
+    let rightAnswer = answers.find((e) => e.is_corect == true);
+    if (rightAnswer) {
+      return true;
+    } else {
+      return false;
     }
   }
 }
